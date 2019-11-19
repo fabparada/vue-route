2d646e69f04a2aaa1b40e05b1d9527d5a00ddb5f
Fabian Parada <fparada@digital.gob.cl>
2019-11-19 16:48:22 -0300

    firs commit

---
+++ src/views/Fotos.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h1>Galeria : </h1>
+        <router-link :to="{name: 'fotos', params: {id : item}}" 
+            v-for="(item, index) of fotosArreglo" :key="index">
+            <button> {{ item }} </button>
+        </router-link>
+        <foto></foto>
+
+        <button class="btn btn-secondary" @click="anterior">anterior</button>
+        <button class="btn btn-success" @click="home">Volver al home</button>
+        <button class="btn btn-secondary" @click="siguiente">siguiente</button>
+
+    </div>
+
+</template>
+
+<script>
+import Foto from "@/components/Fotografia.vue";
+
+export default {
+    name: 'Fotos',
+    components:{
+        Foto
+    },
+    data(){
+        return {
+         fotosArreglo: [1,2,3]
+        }
+    },
+    methods:{
+        home(){
+            this.$router.push('/')
+        },
+        anterior(){
+            this.$router.go(-1)
+        },
+        siguiente(){
+            this.$router.go(1)
+        }
+    }
+}
+</script>
---
+++ src/views/Servicios.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Hola soy un servicio</h1>
+    </div>
+</template>
